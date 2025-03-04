259f07ef6c8d950039f97755acd425f2df09fafd
Darren Wenn <darren.wenn@microchip.com>
2025-03-04 22:11:21 +0000

    lab2 initial commit

---
+++ lab2/lab2.cpp
@@ -0,0 +1,33 @@
+
+#include <memory>
+
+#include <egt/ui>
+#include <egt/uiloader.h>
+ 
+int main(int argc, char** argv)
+{
+    egt::Application app(argc, argv);
+ 
+    egt::experimental::UiLoader loader;
+    auto window = std::static_pointer_cast<egt::Window>(loader.load("file:ui.xml"));
+ 
+    auto spinProgress = window->find_child<egt::v1::SpinProgress>("SpinProgress1");
+
+    egt::v1::PeriodicTimer animateTimer(std::chrono::milliseconds(100));
+    animateTimer.on_timeout([&spinProgress, &animateTimer] ()
+    {
+        spinProgress->value(spinProgress->value() - 5);
+        if (spinProgress->value() == 0)
+            animateTimer.cancel();
+    });
+    
+    auto button = window->find_child<egt::Button>("Button1");
+    button->on_click([&spinProgress, &animateTimer] (egt::Event&)
+    {
+        spinProgress->value(100);
+        animateTimer.start();
+    });
+
+    window->show();
+    return app.run();
+}
